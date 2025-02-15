b1e5da96179ab0191ba0fcbe206eeac3b286f1b1
rmeza09 <rafasmez1009@gmail.com>
2025-02-15 01:17:37 -0500

    updated engineering 02/15/2025

--- src/components/Engineering.js
+++ src/components/Engineering.js
@@ -5,10 +5,20 @@ import NASALogo from "../assets/NASA-Logo.svg.png";
 import JPLLogo from "../assets/Jet_Propulsion_Laboratory_logo.svg.png";
 import AFOSRLogo from "../assets/AFOSR+Logo.png";
 import CaptureImg from "../assets/Capture.PNG";
+
 import TempControl1 from "../assets/IMG_7347.jpg";
 import TempControl2 from "../assets/IMG_7318.jpeg";
 import TempControl3 from "../assets/IMG_7351.jpeg";
 import TempControl4 from "../assets/IMG_7369.jpeg";
+
+import Hermes1 from "../assets/Hermes Assembly.jpg";
+import Hermes2 from "../assets/HermesFinalCadPic.png";
+import Hermes3 from "../assets/IMG_4752 Large.jpeg";
+import Hermes4 from "../assets/IMG_4775.jpg";
+import Hermes5 from "../assets/IMG_4786.jpg";
+import Hermes6 from "../assets/IMG_9954.jpg";
+
+
 //import TempControlVid from "../assets/IMG_7370.mp4";
 import submarinePrint from "../assets/72315635017__05B0C390-E4D2-4AD5-ACB8-7003D435C667.jpg";
 
@@ -23,23 +33,39 @@ function Engineering() {
         <img src={AFOSRLogo} alt="AFOSR" className="company-image" />
       </div>
 
-      <div className="item-container">
-        <Carousel media={[TempControl4, TempControl1, TempControl3, TempControl2, "/videos/IMG_7370.mp4"]} altText="Temperature Controller" />
-        <div className="item-text">
-          <h3>PID Temperature Controller - Spring 2025</h3>
-          <p>A research project focused on solid-state rocketry, performed at the University of Maryland.</p>
-        </div>
+      <div className="engineering-container">
+      
+      {/* ✅ Title & Text on Top */}
+      <h3 className="project-title">PID Temperature Controller - Spring 2025</h3>
+      <p className="project-description">
+        A research project focused on solid-state rocketry, performed at the University of Maryland.
+      </p>
+
+      {/* ✅ Larger Carousel Below */}
+      <Carousel 
+        media={[TempControl4, TempControl1, TempControl3, TempControl2]} 
+        altText="Temperature Controller"
+      />
+
+      </div>
+      
+
+      <div className="engineering-container">
+      
+      {/* ✅ Title & Text on Top */}
+      <h3 className="project-title">HERMES Luna Transport Vehicle - Spring 2023</h3>
+      <p className="project-description">
+        A research project focused on solid-state rocketry, performed at the University of Maryland.
+      </p>
+
+      {/* ✅ Larger Carousel Below */}
+      <Carousel 
+        media={[Hermes1, Hermes5, Hermes6, Hermes3, Hermes4, Hermes2]} 
+        altText="HERMES Project"
+      />
+
       </div>
     
-      <div className="item-container">
-        <img src={TempControl1} alt="Temperature Controller" className="profile-image" />
-        <div className="item-text">
-          <h3>PID Temperature Controller - Spring 2025</h3>
-          <p>
-            A research project focused on solid-state rocketry, performed at the University of Maryland.
-          </p>
-        </div>
-      </div>
 
       <div className="item-container">
         <img src={CaptureImg} alt="Rocket Test Bed" className="profile-image" />
